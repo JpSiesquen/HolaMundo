ecd2952ed2e277c14b46d0e06c68bd332238d668
JpSiesquen <jpsiesquen@gmail.com>
2026-02-10 03:56:42 -0300

    Creación del servicio-categorias

---
+++ productos-services/src/test/java/pe/edu/cibertec/productos/ProductosServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
